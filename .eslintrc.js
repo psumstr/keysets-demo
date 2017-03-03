9c4300a9a1b35477244fc842fa34659c2fb6ae93
Jonathan Avila <avilajonathan@gmail.com>
2017-03-03 13:26:44 -0500

    Add sample React components: `index.js` `components/App.js`
    Add React support in Babel & ESLint:
    `npm install babel-preset-react --save-dev`
    
    Add ES2015 Babel preset:
    `npm install babel-preset-es2015 --save-dev`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
+    "plugins": [
+        "react"
+    ],
     "env": {
         "browser": true,
         "commonjs": true,
         "es6": true,
         "node": true,
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
     "parserOptions": {
         "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
     },
     "rules": {
         "comma-dangle": ["error", "always-multiline"],
